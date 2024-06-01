470d5d0393f4cb81eba36e5ca3276854ae721d74
NicholasTristate <nicholasmeeendez@gmail.com>
2024-06-01 14:14:27 -0400

    Docker configuration finished
    Docker configuration finished
    -PostgresSql added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,8 +7,12 @@ import { RequestLogModule } from './request-log/request-log.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'mydatabase',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
